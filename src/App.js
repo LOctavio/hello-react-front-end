f5c92f09d9c851e4af27ba463e6268280ee9dd24
LOctavio <luis_ram1997@hotmail.com>
2022-02-16 10:10:40 -0600

    Show a random greeting message from an api

--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ import {
 	Link
 } from 'react-router-dom';
 import Greeting from './Greeting';
+import { getGreeting } from './redux/greeting/greeting';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
 function App() {
+
+  const greeting = useSelector((state) => state.greetingReducer);
+  const dispatch = useDispatch();
+
+  const greetingText = () => {
+    axios.get('http://localhost:3000/api/v1/greetings')
+      .then((response) => {
+        const greeting = response.data
+        dispatch(getGreeting(greeting.text));
+      });
+  };
+
   return (
     <div className="App">
       <Router>
         <div className="App">
-          <ul className="App-header">
-            <li>
-              <Link to="/greeting">Greeting</Link>
-            </li>
-          </ul>
+          <div className="App-header">
+            <Link to="/greeting" onClick={greetingText}>Click this link to get a random greeting</Link>
+          </div>
           <Routes>
-                <Route exact path='/greeting' element={< Greeting />}></Route>
+                <Route exact path='/greeting' element={< Greeting greeting={greeting}/>}></Route>
           </Routes>
         </div>
       </Router>
--- src/Greeting.js
+++ src/Greeting.js
@@ -1,5 +1,6 @@
-function Greeting() {
-  return <span>hi</span>
+function Greeting(props) {
+  const {greeting} = props
+  return <p>{greeting}</p>
 }
 
 export default Greeting;
